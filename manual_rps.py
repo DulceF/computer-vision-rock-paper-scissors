db1f345f1bd0d2800eb3db4b81e2aa424a9fcaa5
DulceF <86166245+DulceF@users.noreply.github.com>
2023-03-02 14:50:58 +0000

    Add files via upload

--- manual_rps.py
+++ manual_rps.py
@@ -25,27 +25,27 @@ computer_choice = random.choice(possible_choices)
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
-       print("Both players have chosen {user_choice}. It is a tie!")
+       print("It is a tie!")
 
 #1
     elif user_choice == "rock":
         if computer_choice == "scissors":
-            print("Rock smashes scissors, you won!")
+            print("you won")
         else:
-            print("Paper covers rock, You lost!")
+            print("Paper covers rock, You lost")
 
 #2
     elif user_choice == "paper":
         if computer_choice == "rock":
-            print("Paper covers rock, You won!")
+            print("You won")
         else:
-            print("Rock smashes scissors, You lost!")
+            print("You lost")
  #3   
     elif user_choice == "scissors":
         if computer_choice == "paper":
-            print("Scissors cuts paper, you win!")
+            print("You win")
         else:
-            print("Paper covers rock, You lose!")
+            print("You lost")
 
    
 
